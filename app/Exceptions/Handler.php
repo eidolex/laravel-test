389c7c03de5a669c3fc9cc64354a5267bb45bbcf
Si Thu Hpyo <sandaraion@gmail.com>
2022-12-10 18:47:30 +0630

    override api exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Str;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +47,58 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($request->wantsJson() || Str($request->getPathInfo())->startsWith('/api')) {
+            return $this->renderApiResponse($request, $e);
+        }
+        return parent::render($request, $e);
+    }
+
+    private function renderApiResponse($request, Throwable $e)
+    {
+        $exception = $this->prepareException($e);
+
+
+        $statusCode = 500;
+
+        if ($exception->status) {
+            $statusCode = $exception->status;
+        }
+
+        $response = [
+            'status' => $statusCode,
+        ];
+
+        switch ($statusCode) {
+            case 401:
+                $response['message'] = 'Unauthorized';
+                break;
+            case 403:
+                $response['message'] = 'Forbidden';
+                break;
+            case 404:
+                $response['message'] = 'Not Found';
+                break;
+            case 405:
+                $response['message'] = 'Method Not Allowed';
+                break;
+            case 422:
+                $response['message'] = $exception->getMessage();
+                $response['errors'] = $exception->errors();
+                break;
+            default:
+                $response['message'] = ($statusCode == 500) ? 'Whoops, looks like something went wrong' : $exception->getMessage();
+                break;
+        }
+
+        if (config('app.debug')) {
+            $response['trace'] = $exception->getTrace();
+            $response['code'] = $exception->getCode();
+        }
+
+        return response()->json($response, $statusCode);
     }
 }
