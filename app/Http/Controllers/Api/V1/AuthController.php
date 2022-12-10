b4c89e153ea6f8755c96640fff1eafee8df53ae5
Si Thu Hpyo <sandaraion@gmail.com>
2022-12-10 11:25:34 +0630

    add AuthController with login & register

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Services\AuthService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * @var AuthService
+     */
+    private $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        return $this->authService->login($credentials);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+        return $this->authService->register($data);
+    }
+}
---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                'max:150',
+            ],
+            'email' => [
+                'required',
+                'max:241',
+                'email:filter',
+            ],
+            'password' => [
+                'required',
+                'min:8',
+            ],
+        ];
+    }
+}
