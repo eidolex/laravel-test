7c2eadcb54101161002e385d04722db801b44909
Si Thu Hpyo <sandaraion@gmail.com>
2022-12-10 17:07:32 +0630

    add Form, FormInput Resource

---
+++ app/Http/Resources/FormResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FormResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'form_inputs' => FormInputResource::collection($this->whenLoaded('formInputs')),
+        ];
+    }
+}
